6582fe79ed7f5fc6f704b83b75a8472bc6fe8cd0
splash963 <theekshanahirushan10@gmail.com>
2026-02-19 20:33:08 +0530

    Complete Homepage

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/css/home.css',
+                'resources/js/home.js',
                 'resources/css/layouts/navbar.css',
             ],
             refresh: true,
